d3845b7a879b183f0999feb6f5f6236a3032a8a9
Andy Hawkes <andy@andyhawkes.com>
2018-12-07 11:26:25 -0700

    Add ability to kick off a test.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # loadster-cli
-Loadster Command Line Interface
+
+This is a simple Loadster Command Line Interface, to work with Loadster's
+cloud-hybrid load testing platform at [loadster.app](https://loadster.app).
+
+It's a work in progress!
+
+## Run a Load Test
+
+You can use this utility to kick off load tests from your Loadster
+scenarios. To run a load test, you must first create a scenario in
+your [Loadster Dashboard](https://loadster.app/dashboard/), and obtain
+the **trigger code** unique to that scenario.
+
+To run a load test:
+
+```
+$ loadster run <trigger-code>
+```
+
+Once it starts, you can observe the load test in real time
+from your Loadster dashboard.
--- js/loadster-cli.js
+++ js/loadster-cli.js
@@ -1,6 +1,8 @@
 const Package = require('../package.json')
 const Process = require('process')
 const CommandLineArgs = require('command-line-args')
+const Axios = require('axios')
+const BaseURL = 'https://api.loadsterperformance.com'
 
 const options = CommandLineArgs([
     {
@@ -11,18 +13,20 @@ const options = CommandLineArgs([
         name: 'version',
         alias: 'v',
         type: Boolean
+    },
+    {
+        name: 'json',
+        type: Boolean
     }
 ], {stopAtFirstUnknown: true})
+
+const argv = options._unknown || []
 
 if (options['version']) {
     printVersion()
 } else if (options['command'] === 'run') {
-    const argv = options._unknown || []
-
     if (argv.length > 0) {
-        const testTrigger = argv[0]
-
-        // TODO - run the test
+        runTest(argv[0])
     } else {
         printUsage(1)
     }
@@ -30,14 +34,61 @@ if (options['version']) {
     printUsage(0)
 }
 
+/**
+ * Runs a test by hitting the API with a trigger code.
+ */
+function runTest (triggerCode) {
+    new Promise(async (resolve, reject) => {
+        try {
+            let response = await Axios.post(`${BaseURL}/cloud/triggers/${triggerCode}`)
+
+            resolve(response.data)
+        } catch (err) {
+            reject(new Error(`Unable to run test with trigger code ${triggerCode} (${err.response.status})`))
+        }
+    }).then(result => {
+        if (argv.indexOf('--json') >= 0) {
+            console.log(JSON.stringify(result, null, 2))
+        } else {
+            console.log(`${result.message}\n`)
+
+            if (result.statusUrl) {
+                console.log(`Fetch the current test status as JSON at any time:\n\n${result.statusUrl}\n`)
+            }
+
+            if (result.reportUrl) {
+                console.log(`View the running test or test report in your browser:\n\n${result.reportUrl}\n`)
+            }
+        }
+    }).catch(err => {
+        printError(err.toString(), 2)
+    })
+}
+
+/**
+ * Prints the version and exits.
+ */
 function printVersion () {
     console.log(Package.version)
 }
 
+/**
+ * Prints usage and exits.
+ */
 function printUsage (exitCode) {
     console.log(`This is the command line interface for Loadster's cloud-hybrid testing platform.`)
-    console.log(`Head over to https://loadster.app/dashboard/ to obtain your test triggers.\n`)
-    console.log(`Usage: ${Process.title} run <test-trigger>`)
+    console.log(`Build scenarios and get trigger codes at https://loadster.app/dashboard/`)
+    console.log()
+    console.log(`Usage: ${Process.title} run <trigger-code> [--json]`)
+
+    Process.exitCode = exitCode
+}
+
+/**
+ * Prints an error and exits.
+ */
+function printError (message, exitCode) {
+    console.error(message)
 
     Process.exitCode = exitCode
 }
