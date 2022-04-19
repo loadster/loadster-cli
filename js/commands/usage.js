8fac0bd3a81f84eb20debcb481f63762229a96b8
Andy Hawkes <andy@loadster.app>
2022-04-19 15:43:20 -0700

    Upgrade dependencies and code styles.

--- js/commands/usage.js
+++ js/commands/usage.js
@@ -1,33 +1,37 @@
-const Process = require('process')
+const Process = require('process');
 
 module.exports = (exitCode) => {
-    console.log(`This is the command line interface for Loadster's cloud-hybrid testing platform.`)
-    console.log(`To build Loadster test scenarios and get trigger codes, go to:`)
-    console.log()
-    console.log(`    https://loadster.app/dashboard/`)
-    console.log()
-    console.log(`To start a test and exit:`)
-    console.log()
-    console.log(`    ${Process.title} start <trigger-code> [--json] [--label (str)]`)
-    console.log()
-    console.log(`        --json             Print output in JSON instead of human-friendly`)
-    console.log(`        --label (str)      Label the test for easy identification later`)
-    console.log()
-    console.log(`To run a test, waiting until it finishes:`)
-    console.log()
-    console.log(`    ${Process.title} run <trigger-code> [--json] [--label (str)] [--assert (str)]`)
-    console.log()
-    console.log(`        --json             Print output in JSON instead of human-friendly`)
-    console.log(`        --label (str)      Label the test for easy identification later`)
-    console.log(`        --assert (str)     Assert a value match at the conclusion of a test`)
-    console.log()
-    console.log(`Examples of assertions:`)
-    console.log()
-    console.log(`        --assert 'totalErrors == 0'`)
-    console.log(`        --assert 'totalPages >= 1500'`)
-    console.log(`        --assert 'avgHitsPerSecond > 7.5'`)
-    console.log(`        --assert 'avgBytesPerSecond <= 10000'`)
+  const usage = `
+This is the command line interface for Loadster's cloud-hybrid testing platform.
 
+To build your scripts and scenarios, go to: 
 
-    Process.exitCode = exitCode || 0
-}
+    https://loadster.app/dashboard/
+    
+To start a test and exit:
+
+    ${Process.title} start <trigger-code> [--json] [--label (str)]
+
+        --json             Print output in JSON instead of human-friendly
+        --label (str)      Label the test for easy identification later
+
+To run a test, waiting until it finishes:
+
+    ${Process.title} run <trigger-code> [--json] [--label (str)] [--assert (str)]
+    
+        --json             Print output in JSON instead of human-friendly
+        --label (str)      Label the test for easy identification later
+        --assert (str)     Assert a value match at the conclusion of a test
+
+Examples of assertions:
+
+        --assert 'totalErrors == 0'
+        --assert 'totalPages >= 1500'
+        --assert 'avgHitsPerSecond > 7.5'
+        --assert 'avgBytesPerSecond <= 10000'
+  `.trim();
+
+  console.log(usage + '\n');
+
+  Process.exitCode = exitCode || 0;
+};
--- js/main.js
+++ js/main.js
@@ -1,60 +1,59 @@
-const Process = require('process')
-const Options = require('command-line-args')
+const Process = require('process');
+const Options = require('command-line-args');
 
 const StandardOptions = [
-    {
-        name: 'command',
-        defaultOption: true
-    },
-    {
-        name: 'version',
-        alias: 'v',
-        type: Boolean
-    }
-]
+  {
+    name: 'command',
+    defaultOption: true
+  },
+  {
+    name: 'version',
+    alias: 'v',
+    type: Boolean
+  }
+];
 
-const start = require('./commands/start')
-const run = require('./commands/run')
-const usage = require('./commands/usage')
-const version = require('./commands/version')
+const start = require('./commands/start');
+const run = require('./commands/run');
+const usage = require('./commands/usage');
+const version = require('./commands/version');
 
 const main = async function () {
-    const options = Options(StandardOptions, {stopAtFirstUnknown: true})
-    const argv = options._unknown || []
+  const options = Options(StandardOptions, { stopAtFirstUnknown: true });
+  const argv = options._unknown || [];
 
-    if (options['version']) {
-        await version()
-    } else if (options['command'] === 'start') {
-        if (argv.length > 0) {
-            let runOptions = Options([
-                {name: 'label', type: String},
-                {name: 'json', type: Boolean}
-            ], {argv: argv.slice(1)})
+  if (options['version']) {
+    await version();
+  } else if (options['command'] === 'start') {
+    if (argv.length > 0) {
+      let runOptions = Options([
+        { name: 'label', type: String },
+        { name: 'json', type: Boolean }
+      ], { argv: argv.slice(1) });
 
-            await start(argv[0], runOptions.label, runOptions.json)
-        } else {
-            await usage(1)
-        }
-    } else if (options['command'] === 'run') {
-        if (argv.length > 0) {
-            let runOptions = Options([
-                {name: 'label', type: String},
-                {name: 'json', type: Boolean},
-                {name: 'assert', type: String, multiple: true}
-            ], {argv: argv.slice(1)})
-
-            await run(argv[0], runOptions.label, runOptions.json, runOptions.assert)
-        } else {
-            await usage(1)
-        }
+      await start(argv[0], runOptions.label, runOptions.json);
     } else {
-        await
-            usage(0)
+      await usage(1);
     }
-}
+  } else if (options['command'] === 'run') {
+    if (argv.length > 0) {
+      let runOptions = Options([
+        { name: 'label', type: String },
+        { name: 'json', type: Boolean },
+        { name: 'assert', type: String, multiple: true }
+      ], { argv: argv.slice(1) });
+
+      await run(argv[0], runOptions.label, runOptions.json, runOptions.assert);
+    } else {
+      await usage(1);
+    }
+  } else {
+    await usage(0);
+  }
+};
 
 main().catch(err => {
-    console.error(err.toString())
+  console.error(err.toString());
 
-    Process.exitCode = 1
-})
+  Process.exitCode = 1;
+});
