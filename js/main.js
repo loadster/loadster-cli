369e240df5782e605694433729bdf547b8a4c0b8
Andy Hawkes <andy@loadster.app>
2023-04-24 17:21:03 -0700

    Add a session check before any command that needs it.

--- js/main.js
+++ js/main.js
@@ -43,9 +43,19 @@ const run = require('./commands/run')({ api, axios });
 const projects = require('./commands/projects')({ api, config });
 const usage = require('./commands/usage')();
 const version = require('./commands/version');
-const { die } = require("./utils/control");
+const { die } = require('./utils/control');
 
-const main = async function () {
+async function checkSession () {
+  try {
+    await api.getTeam();
+  } catch (err) {
+    await usage(1, 'login', `You aren't logged in! Log in with '${process.title} login' first.`);
+
+    process.exit(1);
+  }
+}
+
+async function main () {
   const options = args(cliOptions, { stopAtFirstUnknown: true });
   const command = options['command'];
   const argv = options._unknown || [];
@@ -63,11 +73,15 @@ const main = async function () {
         { name: 'json', type: Boolean }
       ], { argv: argv.slice(1) });
 
+      await checkSession();
+
       await start(argv[0], runOptions.label, runOptions.json);
     } else {
       await usage(1, command);
     }
   } else if (options['command'] === 'play') {
+    await checkSession();
+
     const filename = argv[0];
     const projectId = config.getProjectId();
 
@@ -76,6 +90,7 @@ const main = async function () {
     } else if (!projectId) {
       await usage(1, command, `No project configured! Choose a project with '${process.title} projects' before you play a script.`);
     } else {
+      await checkSession();
       await play(filename);
     }
   } else if (options['command'] === 'run') {
@@ -86,6 +101,7 @@ const main = async function () {
         { name: 'assert', type: String, multiple: true }
       ], { argv: argv.slice(1) });
 
+      await checkSession();
       await run(argv[0], runOptions.label, runOptions.json, runOptions.assert);
     } else {
       await usage(1, command);
@@ -94,6 +110,8 @@ const main = async function () {
     const subcommand = argv[0] || 'help';
 
     if (['list', 'use'].includes(subcommand)) {
+      await checkSession();
+
       await projects[subcommand](argv.slice(1));
     } else {
       await usage(1, command);
@@ -101,7 +119,7 @@ const main = async function () {
   } else {
     await usage(0, command);
   }
-};
+}
 
 main().catch(err => {
   die('Operation failed!', err);
